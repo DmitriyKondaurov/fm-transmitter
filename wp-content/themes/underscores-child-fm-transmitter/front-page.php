46b25b2ed739187f0fa8936829a39068afb52cc0
DmitriyKondaurov <invisibl@ukr.net>
2018-01-04 11:08:32 +0200

    start about section
    Signed-off-by: invisible <invisibl@ukr.net>

--- wp-content/themes/underscores-child-fm-transmitter/front-page.php
+++ wp-content/themes/underscores-child-fm-transmitter/front-page.php
@@ -14,8 +14,8 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
             <a href="#" id="nav_down" class="slider_scroll_bottom">
                 <i class="fa fa-angle-down"></i>
             </a>
@@ -24,13 +24,27 @@ get_header(); ?>
                     <div class="brand_name">
                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/new-site.png" alt="Новинка!"
                              class="brand_new">
-                        <h1 class="slogan" >Многофункциональный bluetooth FM модулятор <span
+                        <h1 class="slogan">Многофункциональный bluetooth FM модулятор <span
                                     class="brand">'BT-800'</span></h1>
                     </div>
                     <div class="hero_main">
-                        <div>
-                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/main_img.png" alt="FM модулятор
-                        'BT-800'" class="main_img">
+                        <div id="carousel_1" class="owl-carousel owl-theme">
+                            <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/main_img.png"
+                                      alt="FM
+                            модулятор
+                        'BT-800'" class="main_img"></div>
+                            <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/twoside.png"
+                                      alt="FM
+                            модулятор
+                        'BT-800'" class="main_img"></div>
+                            <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/tfcard.png"
+                                      alt="FM
+                            модулятор
+                        'BT-800'" class="main_img"></div>
+                            <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/charger.png"
+                                      alt="FM
+                            модулятор
+                        'BT-800'" class="main_img"></div>
                         </div>
                         <ul class="hero_shot_spec">
                             <li>Bluetooth hands free (Car Kit)</li>
@@ -42,7 +56,8 @@ get_header(); ?>
                         </ul>
                         <div class="call_to_action">
                             <div class="attention">Акция!</div>
-                            <div>Первым 10-ти покупателям скидка <span class="discount">20%</span></div>
+                            <div class="discount">Первым 10-ти покупателям скидка <span
+                                        class="discount_amount">20%</span></div>
                             <div class="prices"><span class="old_price">550 грн </span> <span class="new_price">440
                                     грн</span></div>
                             <button class="order_button">заказать</button>
@@ -52,15 +67,31 @@ get_header(); ?>
                 </div>
             </section>
             <section class="about">
+                <div class="wrapper">
                 <ul class="about_full_spec">
-                    <li>Безпроводная громкая связь в авто (Bluetooth-Fm)</li>
-                    <li>Безпроводная громкая связь в авто (AUX-Fm)</li>
-                    <li>Трансляция по радио волнам музыки с SD/TF card reader (SD card - Fm)</li>
-                    <li>Трансляция по радио волнам музыки с AUX входа (AUX - Fm)</li>
-                    <li>Трансляция по радио волнам музыки с Bluetooth соединения (A2DP - Fm)</li>
-                    <li>Переходник с двумя выходами USB для умеренной и быстрой зарядки (1A / 2.1A)</li>
-                    <li>Голосовое воспроизведение номера входящих звонков</li>
+                    <li>Безпроводная громкая связь в авто (hands free). С помощью Bluetooth соединения с телефоном и
+                        передачи звука по радио волнам в Fm диапазоне на штатную магнитолу автомобиля.
+                    </li>
+                    <li>Громкая связь в авто (hands free) c AUX входа. С помощью кабельного переходника (выход
+                        наушников 3,5 мм) телефон соединяется с Fm модулятором <span class="brand">'BT-800'</span>
+                        через встроенный AUX вход и передаёт звук по радио волнам в Fm диапазоне на штатную магнитолу
+                        автомобиля.
+                    </li>
+                    <li>Трансляция музыки по радио волнам в Fm диапазоне с встроенного SD/TF card reader на любой
+                        радиоприёмник в радиусе 10 метров.
+                    </li>
+                    <li>Трансляция музыки по радио волнам в Fm диапазоне с любого устройства воспроизведения с выходом
+                        AUX
+                    </li>
+                    <li>Трансляция музыки по радио волнам в Fm диапазоне с любого устройства воспроизведения с
+                        Bluetooth соединением по технологии A2DP
+                    </li>
+                    <li>Голосовое воспроизведение номера входящих звонков (на английском языке)</li>
+                    <li>В комплект поставки входит переходник автомобильной зарядки с двумя выходами USB для умеренной и
+                        быстрой зарядки (1A / 2.1A)
+                    </li>
                 </ul>
+                </div>
             </section>
 
 			<?php // Show the selected frontpage content.
@@ -72,8 +103,8 @@ get_header(); ?>
 				get_template_part( 'template-parts/post/content', 'none' );
 			endif; ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+        </main><!-- #main -->
+    </div><!-- #primary -->
 
 <?php
 get_sidebar();
--- wp-content/themes/underscores-child-fm-transmitter/js/main.js
+++ wp-content/themes/underscores-child-fm-transmitter/js/main.js
@@ -14,8 +14,32 @@ $(window).on("load", function() {
 $(function() {
     // clicking the "down" button will make the page scroll to the $elem's height
     $('#nav_down').click(
-        function (e) {
+        function () {
             $('html, body').animate({scrollTop: $(window).height()}, 800);
         }
     );
+});
+
+$(document).ready(function () {
+    $('#carousel_1').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 10000,
+        items: 1,
+        itemElement: 'div',
+        stageElement: 'div',
+        center: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
 });
