93c0c5ba5d4fdeb528ae13612ed8e888472c274c
invisible <invisibl@ukr.net>
2018-01-19 00:41:11 +0200

    add google analytics ID to <head>:  UA-112738095-1
    Signed-off-by: invisible <invisibl@ukr.net>

--- wp-content/themes/underscores-child-fm-transmitter/header.php
+++ wp-content/themes/underscores-child-fm-transmitter/header.php
@@ -13,7 +13,17 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112738095-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-112738095-1');
+    </script>
+
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 
