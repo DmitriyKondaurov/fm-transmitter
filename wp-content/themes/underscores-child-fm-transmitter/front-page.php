1d464c81d8684855ffd27a353b5e6786f78eadda
invisible <invisibl@ukr.net>
2018-01-20 16:57:44 +0200

    bring back tag manager
    Signed-off-by: invisible <invisibl@ukr.net>

--- wp-content/themes/underscores-child-fm-transmitter/front-page.php
+++ wp-content/themes/underscores-child-fm-transmitter/front-page.php
@@ -68,7 +68,7 @@ get_header(); ?>
                             <div class="prices"><span class="old_price">550 грн </span> <span class="new_price">440
                                     грн</span></div>
                             <button class="b24-web-form-popup-btn-8 order_button"
-                                    onclick="return gtag_report_conversion()">заказать
+                                    onclick="gtag('event', 'click', {'event_category': order_button});">заказать
                             </button>
                             <div>По акции осталось: <span class="left_pcs">6</span> шт.</div>
                         </div>
@@ -248,7 +248,7 @@ get_header(); ?>
                             <div class="prices"><span class="old_price">550 грн </span> <span class="new_price">440
                                         грн</span></div>
                             <button class="b24-web-form-popup-btn-8 order_button"
-                                    onclick="return gtag_report_conversion()">заказать
+                                    onclick="gtag('event', 'click', {'event_category': order_button});">заказать
                             </button>
                             <div>По акции осталось: <span class="left_pcs">6</span> шт.</div>
                         </div>
