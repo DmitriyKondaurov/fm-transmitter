1269bebeee6ee7e6d7b466771244bda85225d248
invisible <invisibl@ukr.net>
2018-09-22 00:53:05 +0300

    rebuild style.css, libs-style.min.css, libs.min.
    Signed-off-by: invisible <invisibl@ukr.net>

---
+++ wp-content/themes/underscores-child-fm-transmitter/fonts/iconmoon/ie7/ie7.js
@@ -0,0 +1,39 @@
+/* To avoid CSS expressions while still supporting IE 7 and IE 6, use this script */
+/* The script tag referencing this file must be placed before the ending body tag. */
+
+/* Use conditional comments in order to target IE 7 and older:
+	<!--[if lt IE 8]><!-->
+	<script src="ie7/ie7.js"></script>
+	<!--<![endif]-->
+*/
+
+(function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+		'icon-voice1': '&#xe900;',
+		'icon-radio': '&#xe901;',
+		'icon-phone_bluetooth_speaker': '&#xe902;',
+		'icon-charg66': '&#xe903;',
+		'icon-card1': '&#xe904;',
+		'icon-bluetooth_connected': '&#xe905;',
+		'icon-aux10': '&#xe906;',
+		'icon-aux5': '&#xe907;',
+		'0': 0
+		},
+		els = document.getElementsByTagName('*'),
+		i, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+}());
