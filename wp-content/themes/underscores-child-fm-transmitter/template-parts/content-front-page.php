e83b3bf5225177b7f836664220e37ab7b3157a2f
Dmytro Kondaurov <invisibl@ukr.net>
2020-11-21 22:02:32 +0200

    edit the content loop

---
+++ wp-content/themes/underscores-child-fm-transmitter/template-parts/content-front-page.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package underscores
+ * @subpackage underscores Child (Fm transmitter)
+ */
+
+?>
+
+	<div class="entry-content">
+		<?php
+		the_content();
+
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'underscores-child-fm-transmitter' ),
+			'after'  => '</div>',
+		) );
+		?>
+        TESTTTTTT
+	</div><!-- .entry-content -->
+
