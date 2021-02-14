25273719cbe9f7689815af8c4516507680ee5e81
Dmytro Kondaurov <invisibl@ukr.net>
2021-02-14 17:57:24 +0200

    dell robots function from functions.php

--- wp-content/themes/underscores-child-fm-transmitter/functions.php
+++ wp-content/themes/underscores-child-fm-transmitter/functions.php
@@ -1,36 +1,5 @@
 <?php
-add_filter('robots_txt', 'add_robotstxt');
-function add_robotstxt($output){
-	$output .= "Allow: $path/wp-content/uploads/\n";
-	$output .= "Disallow: $path/wp-login.php\n";
-	$output .= "Disallow: $path/wp-register.php\n";
-	$output .= "Disallow: $path/xmlrpc.php\n";
-	$output .= "Disallow: $path/template.html\n";
-	$output .= "Disallow: $path/cgi-bin\n";
-	$output .= "Allow: $path/wp-admin/\n";
-//	$output .= "Disallow: $path/wp-includes\n";
-	$output .= "Allow: $path/wp-includes/js/\n";
-	$output .= "Allow: $path/wp-content/plugins\n";
-	$output .= "Disallow: $path/wp-content/cache\n";
-	$output .= "Disallow: $path/wp-content/themes\n";
-	$output .= "Allow: $path/wp-content/themes/underscores-child-fm-transmitter/\n";
-	$output .= "Allow: $path/wp-content/themes/underscores/\n";
-	$output .= "Disallow: *$path/trackback\n";
-	$output .= "Disallow: *$path/feed\n";
-	$output .= "Disallow: */comments\n";
-	$output .= "Disallow: *$path/comment-page*\n";
-	$output .= "Disallow: *$path/replytocom=\n";
-	$output .= "Disallow: $path/author*\n";
-	$output .= "Disallow: *$path/?author=*\n";
-	$output .= "Disallow: *$path/tag\n";
-	$output .= "Disallow: $path/?feed=\n";
-	$output .= "Disallow: $path/?s=\n";
-	$output .= "Disallow: $path/?se=\n";
 
-	$output .= "Host: shops.sytes.net\n";
-
-	return $output;
-}
 function underscores_child_scripts() {
 	wp_enqueue_style( 'underscores-style', get_stylesheet_uri() );
 
