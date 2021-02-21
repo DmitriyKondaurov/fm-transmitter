3dc8bcb64abc9b93ec5584837385400e0f930529
Dmytro Kondaurov <invisibl@ukr.net>
2021-02-21 19:22:44 +0200

    add some custom options

--- wp-content/themes/underscores-child-fm-transmitter/includes/carbon-fields-options/carbon-fields-options.php
+++ wp-content/themes/underscores-child-fm-transmitter/includes/carbon-fields-options/carbon-fields-options.php
@@ -8,11 +8,28 @@ use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
 Container::make( 'theme_options', 'Site Settings' )
-		->add_tab( 'Logo', [
-		 Field::make( 'image', 'site_logo', 'Logo-image' ),
-		 Field::make( 'text', 'site_logo_text', 'Logo-text' ),
-		 Field::make( 'text', 'text_brand', 'Logo-brand' ),
-		] )
-		->add_tab( 'Viber phone', [
-			Field::make( 'text', 'viber_phone', 'Viber-contact' ),
-		] );
+	->add_tab( 'General options', [
+		Field::make( 'text', 'brand_name', 'Brand_name' ),
+		Field::make( 'color', 'brand_color', __( 'Background Color' ) ),
+		Field::make( 'checkbox', 'show_content', __( 'Show Content' ) ),
+		Field::make( 'checkbox', 'crb_show_content', 'Show content' ),
+		Field::make( 'rich_text', 'crb_content', 'Content' )
+		     ->set_conditional_logic( array(
+			     array(
+				     'field' => 'crb_show_content',
+				     'value' => true,
+			     )
+		     ) ),
+			] )
+
+	->add_tab( 'Site header', [
+		Field::make( 'image', 'site_logo', 'Logo-image (output size = 28☓28)' ),
+		Field::make( 'text', 'site_logo_text', 'Logo-text' ),
+		Field::make( 'text', 'viber_phone', 'Viber-contact' )
+		     ->set_attribute( 'placeholder', '+☓☓ (☓☓☓) ☓☓☓-☓☓-☓☓' )
+		     ->set_attribute( 'type', 'phone' ),
+	] )
+	->add_tab( 'Site Hero', [
+		Field::make( 'text', 'brand_full_name', 'Brand_full_name' )
+	] )
+;
